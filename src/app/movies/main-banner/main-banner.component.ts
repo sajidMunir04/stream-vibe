36157bf26fa92b89cb9743175593f86de17023b6
SAJID <sajid.munir4428@hotmail.com>
2024-05-16 22:18:23 +0500

    updates

---
+++ src/app/movies/main-banner/main-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-banner.component.html',
+  styleUrl: './main-banner.component.css'
+})
+export class MainBannerComponent {
+
+}
--- src/app/pages/movies-and-shows/movies-and-shows.component.ts
+++ src/app/pages/movies-and-shows/movies-and-shows.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { MainBannerComponent } from '../../movies/main-banner/main-banner.component';
 
 @Component({
   selector: 'app-movies-and-shows',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,FooterComponent,MainBannerComponent],
   templateUrl: './movies-and-shows.component.html',
   styleUrl: './movies-and-shows.component.css'
 })
